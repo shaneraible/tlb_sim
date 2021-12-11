f4d8c41e47055eed8606418e60d4d4ff6c84447b
ssraible@gmail.com <ssraible@gmail.com>
2021-12-11 18:13:02 -0500

    changeable TLB size

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tlb_sim
+This project simulates a Translation Lookaside Buffer given the address trace file spice.din. 
+
+To run
+```
+python3 tlb.py
+```
+
+The ouput is a breakdown of the hit rate and the resulting translation lookaside buffer for each data and instruction accesses. The replacement policy used is LRU and is implemented used an OrderedDict. The backbone of this is a hashmap that contains references to nodes in a doublely linked list. Every time the memory is accessed we move it to the top of the dict.
--- tlb.py
+++ tlb.py
@@ -3,7 +3,7 @@ from os import access
 import time 
 
 class TLB: 
-    def __init__(self, capacity):
+    def __init__(self, capacity=32):
         self.cache = OrderedDict()
         self.capacity = capacity
         self.curr_ppn = 0
@@ -111,11 +111,29 @@ def execute_instruction(line):
 
 
 def main():
+    tlb_size = 0
+    valid_in = False
+    while not valid_in:
+        try:
+            in_tlb_size = input("Enter TLB size (defualt 32): ")
+            tlb_size = int(in_tlb_size)
+        except:
+            print('Error: please enter a positive integer.')
+        else:
+            if tlb_size>=0: valid_in = True
+            else: print('Error: please enter a positive integer.')
+
+    GVars.i_tlb = TLB(tlb_size)
+    GVars.d_tlb = TLB(tlb_size)
+
     start = time.time()
     with open('spice.din') as f:
         lines = f.readlines()
+        i = 0
         for line in lines:
             execute_instruction(line)
+            i+=1
+
     end = time.time()
     d_hit_rate = GVars.d_hits / GVars.d_accesses
     i_hit_rate = GVars.i_hits / GVars.i_accesses
@@ -127,10 +145,17 @@ def main():
     print('-----------------------------------------')
     print(f'executed in {end-start} seconds')
     
-    print(f'\nData hit rate: {d_hit_rate}')
+    print(f'\nData accesses:\t {GVars.d_accesses}')
+    print(f'Data hits:\t {GVars.d_hits}')
+    print(f'Data misses:\t {GVars.d_misses}')
+    print(f'Data hit rate:\t {d_hit_rate}')
     print(f'Data page walks: {GVars.d_pw}')
-    print(f'\nInstruction hit rate: {i_hit_rate}')
-    print(f'Instruction page walks: {GVars.i_pw}')
+
+    print(f'\nInstruction accesses:\t {GVars.i_accesses}')
+    print(f'Instruction hits:\t {GVars.i_hits}')
+    print(f'Instruction misses:\t {GVars.i_misses}')
+    print(f'Instruction hit rate:\t {i_hit_rate}')
+    print(f'Instruction page walks:\t {GVars.i_pw}')
 
     print(f'\nTotal hit rate: {hit_rate}')
     print(f'\nTotal pages: {GVars.curr_ppn}')
