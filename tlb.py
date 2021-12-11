0be646021f62b72ee312b38090c89396618765b4
ssraible@gmail.com <ssraible@gmail.com>
2021-12-11 18:44:44 -0500

    optional separate TLBs

--- tlb.py
+++ tlb.py
@@ -142,8 +142,22 @@ def main():
             if tlb_size>=0: valid_in = True
             else: print('Error: please enter a positive integer.')
 
-    GVars.i_tlb = TLB(tlb_size)
-    GVars.d_tlb = TLB(tlb_size)
+    valid_in = False
+    inp = ''
+    while not valid_in:
+        inp = input("Data and Instruction TLB together (t) or separate (s): ").lower()
+    
+        if inp == 't' or inp == 's': valid_in = True
+        else: print('Error: please enter t or s.')
+    
+    together = inp == 't'
+
+    if together:
+        GVars.d_tlb = TLB(tlb_size)
+        GVars.i_tlb = GVars.d_tlb
+    else:
+        GVars.d_tlb = TLB(tlb_size)
+        GVars.i_tlb = TLB(tlb_size)
 
     start = time.time()
     with open('spice.din') as f:
@@ -154,42 +168,64 @@ def main():
             i+=1
 
     end = time.time()
-    d_hit_rate = GVars.d_hits / GVars.d_accesses
-    i_hit_rate = GVars.i_hits / GVars.i_accesses
-    hit_rate = (GVars.i_hits + GVars.d_hits) / (GVars.i_accesses + GVars.d_accesses)
 
-    print('\n-----------------------------------------')
+    if not together:
+        d_hit_rate = GVars.d_hits / GVars.d_accesses
+        i_hit_rate = GVars.i_hits / GVars.i_accesses
+        hit_rate = (GVars.i_hits + GVars.d_hits) / (GVars.i_accesses + GVars.d_accesses)
 
-    print('\t\tSUMMARY')
-    print('-----------------------------------------')
-    print(f'executed in {end-start} seconds')
+        print('\n-----------------------------------------')
+
+        print('\t\tSUMMARY')
+        print('-----------------------------------------')
+        print(f'executed in {end-start} seconds')
+        
+        print(f'\nData accesses:\t {GVars.d_accesses}')
+        print(f'Data hits:\t {GVars.d_hits}')
+        print(f'Data misses:\t {GVars.d_misses}')
+        print(f'Data hit rate:\t {d_hit_rate}')
+        print(f'Data page walks: {GVars.d_pw}')
+
+        print(f'\nInstruction accesses:\t {GVars.i_accesses}')
+        print(f'Instruction hits:\t {GVars.i_hits}')
+        print(f'Instruction misses:\t {GVars.i_misses}')
+        print(f'Instruction hit rate:\t {i_hit_rate}')
+        print(f'Instruction page walks:\t {GVars.i_pw}')
+
+        print(f'\nTotal hit rate: {hit_rate}')
+        print(f'\nTotal pages: {GVars.curr_ppn}')
+        print('-----------------------------------------\n')
+
+        print('Final instruction TLB')
+        print('vpn     : ppn')
+        i = 0
+        for entry in GVars.i_tlb.cache:
+            print(f'{hex(entry)}: {hex(GVars.i_tlb.cache[entry])}')  
+
+        print('\nFinal data TLB')
+        print('vpn       : ppn')
+        for entry in GVars.d_tlb.cache:
+            print(f'{hex(entry)}: {hex(GVars.d_tlb.cache[entry])}')
     
-    print(f'\nData accesses:\t {GVars.d_accesses}')
-    print(f'Data hits:\t {GVars.d_hits}')
-    print(f'Data misses:\t {GVars.d_misses}')
-    print(f'Data hit rate:\t {d_hit_rate}')
-    print(f'Data page walks: {GVars.d_pw}')
+    else:
+        hit_rate = (GVars.i_hits + GVars.d_hits) / (GVars.i_accesses + GVars.d_accesses)
+        print('\n-----------------------------------------')
 
-    print(f'\nInstruction accesses:\t {GVars.i_accesses}')
-    print(f'Instruction hits:\t {GVars.i_hits}')
-    print(f'Instruction misses:\t {GVars.i_misses}')
-    print(f'Instruction hit rate:\t {i_hit_rate}')
-    print(f'Instruction page walks:\t {GVars.i_pw}')
+        print('\t\tSUMMARY')
+        print('-----------------------------------------')
+        print(f'executed in {end-start} seconds')
+        
+        print(f'\Aaccesses:\t {GVars.d_accesses + GVars.i_accesses}')
+        print(f'Hits:\t {GVars.d_hits + GVars.i_hits}')
+        print(f'Misses:\t {GVars.d_misses + GVars.i_misses}')
+        print(f'Hit rate:\t {hit_rate}')
+        print(f'Page walks: {GVars.d_pw + GVars.i_pw}')
+        print('-----------------------------------------\n') 
 
-    print(f'\nTotal hit rate: {hit_rate}')
-    print(f'\nTotal pages: {GVars.curr_ppn}')
-    print('-----------------------------------------\n')
-
-    print('Final instruction TLB')
-    print('vpn     : ppn')
-    i = 0
-    for entry in GVars.i_tlb.cache:
-        print(f'{hex(entry)}: {hex(GVars.i_tlb.cache[entry])}')  
-
-    print('\nFinal data TLB')
-    print('vpn       : ppn')
-    for entry in GVars.d_tlb.cache:
-        print(f'{hex(entry)}: {hex(GVars.d_tlb.cache[entry])}')
+        print('\nFinal TLB')
+        print('vpn       : ppn')
+        for entry in GVars.d_tlb.cache:
+            print(f'{hex(entry)}: {hex(GVars.d_tlb.cache[entry])}')
 
     print('-----------------------------------------\n')
 
